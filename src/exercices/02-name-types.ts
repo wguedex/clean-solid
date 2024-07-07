8071f1cedd95ff23e5b240944221d51e70031e47
Wilmer Guedez <w.guedex@gmail.com>
2024-07-07 10:05:53 -0500

    nombres segun tipo

---
+++ src/exercices/02-name-types.ts
@@ -0,0 +1,92 @@
+
+//Buenas practicas
+(() => {
+ 
+    const temperaturesCelsius = [33.6, 12.34];
+ 
+    const serverIp = '123.123.123.123';
+
+    const users = [{id: 1, email: 'fernando@google.com'},{ id: 2, email: 'juan@google.com' }, { id: 3, email: 'melissa@google.com' }];
+
+    const userEmails = users.map( user => user.email );
+
+    const canJump = false;
+    const canRun = true;
+    const hastItems = false;
+    const isloading = true;
+
+    const startTime = new Date().getTime();
+
+    const endTime = new Date().getTime() - start;
+
+    function getBooks() {
+        throw new Error('Function not implemented.');
+    }
+
+    function getBooksByUrl( url: string) {
+        throw new Error('Function not implemented.');
+    }
+
+    function getSquareAre( side: number ) {
+        throw new Error('Function not implemented.');
+    }
+
+    function printJob() {
+        throw new Error('Function not implemented.');
+    }
+      
+})();
+
+//Malas practicas
+// (() => {
+
+//     // arreglo de temperaturas celsius
+//     const arrayOfNums = [33.6, 12.34];
+
+//     // Dirección ip del servidor
+//     const ip = '123.123.123.123';
+
+//     // Listado de usuarios
+//     const people = [{id: 1, email: 'fernando@google.com'},{ id: 2, email: 'juan@google.com' }, { id: 3, email: 'melissa@google.com' }];
+
+//     // Listado de emails de los usuarios
+//     const emails = people.map( u => u.email );
+
+//     // Variables booleanas de un video juego
+//     const jump = false;
+//     const run = true;
+//     const noTieneItems = true;
+//     const loading = false;
+
+//     // Otros ejercicios
+//     // tiempo inicial
+//     const start = new Date().getTime();
+//     //....
+//     // 3 doritos después
+//     //...
+//     // Tiempo al final
+//     const end = new Date().getTime() - start;
+
+
+//     // Funciones
+//     // Obtiene los libros
+//     function book() {
+//         throw new Error('Function not implemented.');
+//     }
+
+//     // obtiene libros desde un URL
+//     function BooksUrl( u: string) {
+//         throw new Error('Function not implemented.');
+//     }
+    
+//     // obtiene el área de un cuadrado basado en sus lados
+//     function areaCuadrado( s: number ) {
+//         throw new Error('Function not implemented.');
+//     }
+
+//     // imprime el trabajo
+//     function printJobIfJobIsActive() {
+//         throw new Error('Function not implemented.');
+//     }
+      
+// })();
